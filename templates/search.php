873e5f1e01b13d5f1418f421d390afd6f4ca7b3c
Werdlem <smrobins@virginmedia.com>
2016-10-15 11:55:54 +0100

    Activity
    Link from product search page to product activity page fixed.
    results.php now reads correct

--- templates/search.php
+++ templates/search.php
@@ -142,7 +142,6 @@ foreach ($goods_total as $total_sku){
 						echo '<a href="?action=action&clear_location&location_id='.$result['location_id'].'">Delete</a>';
 				?>
                 | <a href="?action=send&sku_order=<?php echo $result['sku']?>&qty=<?php echo $result['pack_qty'];?>">Order</a>
-                | <a href='?action=results&sku="<?php echo $result['sku']?>"'>Link</a>
 				<?php if ($result['sku']>0){
 					echo "| <a href='?action=update_product&sku=".$result['sku']."&sku_id=". $result['sku_id']."'>Details</a>";
 					
