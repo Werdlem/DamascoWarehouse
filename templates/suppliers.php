2889512ff16a22f4beb27e0df464746d5cda6f26
Werdlem <smrobins@virginmedia.com>
2017-02-02 15:30:40 +0000

    V17.05
    search dates and supplier in the text boxes now echo the searched
    criteria.

--- templates/suppliers.php
+++ templates/suppliers.php
@@ -1,6 +1,15 @@
 <?php 
 require_once './DAL/PDOConnection.php';
-$suppliers = new products; 
+$suppliers = new products;
+if(isset($_POST['doSearch'])){ 
+ 
+ $dateFrom = $_POST['date-from'];
+ $dateTo = $_POST['date-to'];
+ $supplier_name = $_POST['taskOption'];
+}
+ else{ $dateFrom = 'Date From';
+ $dateTo = 'Date To';
+$supplier_name = 'Select Supplier';}
 ?>
 
 <div class="panel panel-primary">
@@ -10,7 +19,7 @@ $suppliers = new products;
   <div class="panel-body">
     <form method="post" action="" id="Search">
       <select name="taskOption">
-        <option value="dnu">Select supplier</option>
+        <option><?php echo $supplier_name ?></option>
         <option>Manchester Paper box</option>
         <option>John Roberts</option>
         <option>Krystals</option>
@@ -23,8 +32,8 @@ $suppliers = new products;
         <option>Sansetsu</option>
         <option>Smurfit Kappa</option>
       </select>
-      <input class="suppliers" name="date-from" placeholder=" date from" type="text" onfocus="(this.type='date')" />
-      <input class="suppliers" name="date-to" placeholder=" date to" type="text" onfocus="(this.type='date')" />
+      <input class="suppliers" name="date-from" value="<?php echo $dateFrom ?>" type="text" onfocus="(this.type='date')" />
+      <input class="suppliers" name="date-to" value="<?php echo $dateTo ?>" type="text" onfocus="(this.type='date')" />
       <button type="submit" class="btn btn-large btn-success" name="submit">Search</button>
       <input type="hidden" name="doSearch" value="1"  />
     </form>
@@ -32,7 +41,7 @@ $suppliers = new products;
     <?php
   if(isset($_POST['doSearch'])){
 	  
-	  if ($_POST['taskOption'] == "dnu") {die ("please select a supplier");} else{
+	  if ($_POST['taskOption'] == "Select Supplier") {die ("please select a supplier");} else{
   
  
  $dateFrom = $_POST['date-from'];
@@ -100,7 +109,7 @@ $suppliers = new products;
 	  echo '<p>'. $late . " <span class='label label-danger'> Late</span> Deliveries (" . number_format((100.0*$late)/$count ) . "%)";
 	 
 }?>
-      <div class="alert alert-info" role="alert" style="width:75%; float: right; margin-top: -102px; font-size:13px; padding:10px"><strong>On Time/Late/Early</strong> deliveries is the difference between the initial agreed <strong>Due Date</strong> & actual <strong>Delivery Date</strong>. The <strong>Margin</strong> is the difference of delivery time between the <strong>Scheduled Date & Time</strong> and actual <strong> Delivery Date & Time</strong>. This result is then compared to a 90minute delivery grace period, going green if within the 90minutes or red if later.</div>
+      <div class="alert alert-info" role="alert" style="width:75%; float: right; margin-top: -102px; font-size:13px; padding:10px"><strong>On Time/Late/Early</strong> deliveries is the difference between the initial agreed <strong>Due Date</strong> & actual <strong>Delivery Date</strong>. The <strong>Margin</strong> is the difference of delivery time between the <strong>Scheduled Date & Time</strong> and actual <strong> Delivery Date & Time</strong>. This result is then compared to a 90 minute delivery grace period, going green if within the 90minutes or red if later.</div>
       <?php }?>
         </tbody>
       
