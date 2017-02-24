7aced961e637a82cd70f8f5341dc2fcf1962ead4
Werdlem <smrobins@virginmedia.com>
2017-02-24 07:49:00 +0000

    V17.10
    Added update button to stock order report to execute the updateStock
    stored procedure.

--- templates/_stock_order_report.php
+++ templates/_stock_order_report.php
@@ -1,28 +1,41 @@
 <?php require_once './DAL/PDOConnection.php';
 $productDal = new products();
 $goods_total = $productDal->get_stock_order_report();
-			?>
-            <style>
+
+// check to see if the page is posted back via the "Update button" then continue to load the rest of the page
+
+if(isset($_POST['doSearch'])){
+	
+	if($_POST['doSearch']==1)
+		{
+			$update = $productDal->sku_qty_update();
+			header("location:?action=_stock_order_report");
+		}
+}
+else{
+		?>
+		 <style>
 			
 			.ordered{display:none; }
 			.not_ordered{background-color: rgba(255,0,0,0.2); }
 			
 			</style>
+
             <div class="panel panel-primary" style="width:100%; float:left;">
 <div class="panel-heading" style="text-align:center;">
   <h3>Low Stock Order Report</h3>
 </div>
 <div class="panel-body">
 <div>
-<table class="table">
-  <tr class="heading">
-    <td style="font-size:16px"><strong>SKU</td></strong>
-    <td style="font-size:16px; text-align:center"><strong>Date Ordered</td></strong>
-    <td style="font-size:16px; text-align:center"><strong>Date Rec</td></strong>
-    <td style="font-size:16px; text-align:center"><strong>SKU Total</td></strong>
-    <td style="font-size:16px; text-align:center; background-color: rgba(0,0,255,0.3);"><strong>Buffer Qty</td></strong>
-    <td style="font-size:16px; text-align:center"><strong>Order</td></strong>
-  </tr>
+	<table class="table">
+ 	 <tr class="heading">
+   		 <td style="font-size:16px"><strong>SKU</td></strong>
+   		 <td style="font-size:16px; text-align:center"><strong>Date Ordered</td></strong>
+   		 <td style="font-size:16px; text-align:center"><strong>Date Rec</td></strong>
+    	 <td style="font-size:16px; text-align:center"><strong>SKU Total</td></strong>
+    	 <td style="font-size:16px; text-align:center; background-color: rgba(0,0,255,0.3);"><strong>Buffer Qty</td></strong>
+     	 <td style="font-size:16px; text-align:center"><strong>Order</td></strong>
+  	</tr>
   </td>
   <?php
   foreach ($goods_total as $result){
@@ -31,8 +44,8 @@ $goods_total = $productDal->get_stock_order_report();
 			?>
     <td style=""><a href="?action=activity&sku=<?php echo htmlspecialchars($result['sku']);?>"><?php echo htmlspecialchars($result['sku']); ?></a></td>
   <?php  			
-	 echo '<td class="sean">'. date('d-m-Y', strtotime($result['last_order_date']));		
-	 echo'<td class="sean">'. date('d-m-Y', strtotime($result['delivery_date']));
+	 echo '<td style="text-align:center">'. date('d-m-Y', strtotime($result['last_order_date']));		
+	 echo'<td style="text-align:center">'. date('d-m-Y', strtotime($result['delivery_date']));
 	?>
     </strong>
    
@@ -42,12 +55,18 @@ $goods_total = $productDal->get_stock_order_report();
 		echo '<td style="text-align:center;"><a href="?action=send&sku_order='.htmlspecialchars($result['sku']) .'&qty=' .$result['pack_qty'].'"class="btn btn-default btn-primary">Order</a></td>';
 						
 		}
-
+}
 
 ?>
   </tr>
 </table>
 </div>
 </div>
+</div>
 
+<form action="?action=_stock_order_report" method="post" location_id="Search">
+<input type="submit" class="btn btn-large btn-success" name="Submit" value="Update" />
+<input type="hidden" name="doSearch" value="1">
+
+</form>
 
