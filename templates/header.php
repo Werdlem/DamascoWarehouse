404b5512a51b19bf52bfcfb0917700da97f6647c
Werdlem <smrobins@virginmedia.com>
2017-06-28 09:44:47 +0100

    V17.45
    footer update and headder update

--- templates/header.php
+++ templates/header.php
@@ -47,11 +47,13 @@
 
             </ul>
            <li><a href="?action=suppliers">Suppliers</a></li>
-         <?php }?>
+       
          <li role="presentation" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="?action=ctn_carton_specs">Carton Production</a>
             <ul class="dropdown-menu" role="menu">
               <li><a href="?action=ctn_jobSheet">Job Sheets</a></li>
+               <li><a href="?action=production">Production Log</a></li>
         </ul>
+        <?php }?>
       </div>
     </ul>
   </div>
