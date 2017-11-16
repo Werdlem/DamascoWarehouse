a0d08800be4b8ab49507116c783d0bdfba774134
Werdlem <smrobins@virginmedia.com>
2017-11-16 10:43:08 +0000

    v17.95
    changed the 'from' email address to purchasing@postpack for visibility
    and reply purposes

--- templates/send.php
+++ templates/send.php
@@ -1,6 +1,8 @@
  <?php
  require_once "DAL/settings.php";
  require_once 'lib/swift_required.php';
+
+ $EMAIL_ORDERS_PU = 'purchasing@postpack.co.uk';
  
  if (isset($_GET['sku'])){
 	 require_once 'DAL/PDOConnection.php';
@@ -25,19 +27,17 @@
 	 $qty = $_GET['qty']; 
 	 $sku_id = $_GET['id'];
 	 date_default_timezone_set('UTC');
-	$today = date('Y-m-d');
+	 $today = date('Y-m-d');
 
 	 }
 	   
  if (isset($_GET['production_product'])){
 	 require_once 'DAL/Production_PDOConnection.php';
 	 $productionDal = new products();
-	 
 	 $product = $_GET['production_product'];	 
 	 date_default_timezone_set('UTC');
 	$today = date('d-m-y');
-   
-		$productionDal->updateProduct($product, $today);
+	$productionDal->updateProduct($product, $today);
 	 
 	 }
 	 else
@@ -65,8 +65,8 @@
 			$mailer = Swift_Mailer::newInstance($transport);			
 			$message = Swift_Message::newInstance('Please Order')
 			->setSubject('Product Order: ' .$product)
-			->setFrom($EMAIL_ORDERS_FROM)
-			->setCc($EMAIL_ORDERS_CC)
+			->setFrom($EMAIL_ORDERS_PU)
+			->setCc($EMAIL_ORDERS_PU)
 			->setTo($EMAIL_ORDERS_TO)
 			
 			//Order Body//
