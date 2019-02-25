1e55c9f9561749c182559afd6355f03f415f155f
Werdlem <smrobins@virginmedia.com>
2019-02-25 17:12:31 +0000

    v19.01

--- templates/couriers.php
+++ templates/couriers.php
@@ -1,16 +1,27 @@
 
 <div ng-controller="couriers as c" ng-app="sheetBoard">
-	<input type="date" ng-model="start">
-	<input type="date" ng-model="end">
+	<h1>Couriers</h1>
+	<p>Date from: <input type="date" ng-model="start">
+	Date to: <input type="date" ng-model="end">
 	
-	<button ng-click="search()">Search</button>
-	<h2>Hello world</h2>
+	<button ng-click="search()" class="btn btn-large btn-success">Search</button>
 
-	<tr ng-repeat="x in c.getData">
-		<td>{{x.count}}</td>
+
+<div>
+<table class="table" style="width: 50%">
+	<tr>
+		<th>Courier</th>
+		<th>Consignments</th>
+		<th>Boxes</th>
 	</tr>
-	
+	<tr ng-repeat="x in c.getData">
+		<td>{{x.courier | uppercase}}</td>
+		<td>{{x.count}}</td>
+		<td>{{x.labels | number: 0}}</td>
+	</tr>
+	</table>
 
 
 <script src="/restricted/myApp.js"></script>
+</div>
 </div>
