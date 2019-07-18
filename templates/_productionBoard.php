2114abed55611e6c81c0e211fa462303d6921c55
Werdlem <smrobins@virginmedia.com>
2019-07-18 10:42:37 +0100

    v19.02

--- templates/_productionBoard.php
+++ templates/_productionBoard.php
@@ -1,3 +1,8 @@
+<div id="colourKey" style="width: 8%; padding-left: 5px; text-align: center; font-size: 1.5em;position: fixed; margin-left: -200px; font-weight: bold">
+			<p style="background-color: rgba(255,0,0,0.1)">Below Buffer </p>
+			<p style="background-color: rgba(0,255,0,0.1) ">On Order</p>
+			<p style="background-color: rgba(0,0,255,0.1) ">In Stock</p>
+		</div>
 <?php require_once './DAL/PDOConnection.php';
 $productDal = new products();
 $goods_total = $productDal->getProductionBoard();
@@ -15,10 +20,15 @@ if(isset($_POST['update'])){
 else{
 		
 		?>
+
+		
 		 <style>
 			
 			.ordered{background-color: rgba(0, 255, 0, 0.2); }
 			.not_ordered{background-color: rgba(); }
+			.low{background-color: rgba(255,0,0,0.1);}
+			.inStock{background-color: rgba(0, 0, 255, 0.1); }
+			.dno{display: none}
 			
 			</style>
 
@@ -41,20 +51,24 @@ else{
   <?php
   foreach ($goods_total as $result){
 	 $status = $result['last_order_date'] > $result['delivery_date']? 'ordered': 'not_ordered';
+	 $low =  $result['buffer_qty'] > $result['stock_qty']? 'low':'ok';
+	 $inStock =  $result['stock_qty']> $result['buffer_qty']? 'inStock':'na';
+	$dno = $result['allocation_id']> 29 ? 'order':'dno';
 
-		echo "<tr class='$status'>";
+
+		echo "<tr class='$status $low $inStock'>";
 			?>
-    <td style=""><a href="?action=activity&sku=<?php echo htmlspecialchars($result['sku']).'&sku_id='.$result['sku_id'];?>"><?php echo htmlspecialchars($result['sku']); ?></a></td>
+    <td class=""><a href="?action=activity&sku=<?php echo htmlspecialchars($result['sku']).'&sku_id='.$result['sku_id'];?>"><?php echo htmlspecialchars($result['sku']); ?></a></td>
   <?php  			
-	 echo '<td style="text-align:center">'. date('d-m-Y', strtotime($result['last_order_date']));		
-	 echo'<td style="text-align:center">'. date('d-m-Y', strtotime($result['delivery_date']));
+	 echo "<td class='' style='text-align:center'>". date('d-m-Y', strtotime($result['last_order_date']));		
+	 echo"<td style='text-align:center' class=''>". date('d-m-Y', strtotime($result['delivery_date']));
 	?>
     </strong>
    
 		<?php 
-		echo '<td style="text-align:center; "><strong style="color: red; ">'. $result['stock_qty'];
-		echo '<td style="text-align:center; background-color: rgba(255,0,0,0.1); ">'. $result['buffer_qty'];
-		echo '<td style="text-align:center;"><a href="?action=send&sku_order='.htmlspecialchars($result['sku']) .'&qty=' .$result['pack_qty'].'&id='.$result['sku_id'].'"class="btn btn-default btn-primary">Order</a></td>';
+		echo "<td class='' style='text-align:center; '><strong style='color: red;'>". $result['stock_qty'];
+		echo "<td class='' style='text-align:center;'>". $result['buffer_qty'];
+		echo "<td style='text-align:center;' class='$dno'><a href='?action=send&sku_order='".htmlspecialchars($result['sku']) ."'&qty='" .$result['pack_qty']."'&id='".$result['sku_id']."'class='btn btn-default btn-primary'>Order</a></td>";
 						
 		}
 }
@@ -65,6 +79,4 @@ else{
 </div>
 </div>
 </div>
-
-
 
