7aea76ea1662bae2372276ef0f3f11e5057ba0b8
Werdlem <smrobins@virginmedia.com>
2020-05-26 09:49:16 +0100

    couriers

--- jsonData/couriers.json.php
+++ jsonData/couriers.json.php
@@ -4,6 +4,12 @@ $value = json_decode(file_get_contents("php://input"));
 $start = $value->start;
 $end = $value->end;
 
+$start = new DateTime($value->start);
+$end = new DateTime($value->end);
+$scheduleDate->setTimezone(new DateTimeZone('Europe/London'));
+$start = $start ->format('Y-m-d H:i:s');
+$end = $start ->format('Y-m-d H:i:s');
+
 $results = json_decode(file_get_contents("http://postpack.web/reports/couriers.php?start=".$start."&end=".$end));
 
 echo json_encode($results);
